f17d91a737ecbf393a447838396f818464e5795f
sasahara87 <130122533+sasahara87@users.noreply.github.com>
2023-10-12 13:00:52 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// ハンバーガーメニュー
+$(function(){
+    $('.humburger-btn').on('click',function(){
+        $('.humberger-menu').toggleClass('show')
+    })
+})
+
+
+
+// フェードイン
+$(function(){
+    $('.skill-text,.service-item').on('inview',function(){
+        $(this).addClass('fadeIn')
+    })
+})
+
+
+// 画像のスライダー
+$(function(){
+    $('.slider1').slick({
+        autoplay:true,
+        autoplaySpeed:0,
+        speed:3000,
+        cssEase:"linear",
+        slidesToShow:5,
+        swipe:false,
+        arrows:false,
+        responsive:[
+            {
+                breakpoint:599,
+                settings:{
+                    slidesToShow:3,
+
+            }
+        }
+        ]
+    })
+})
+
+
+$(function(){
+    $('.slider2').slick({
+        autoplay:true,
+        autoplaySpeed:0,
+        speed:3000,
+        cssEase:"linear",
+        slidesToShow:5,
+        swipe:false,
+        arrows:false,
+        rtl:true,
+        responsive:[
+            {
+                breakpoint:599,
+                settings:{
+                    slidesToShow:3,
+
+            }
+        }
+        ]
+    })
+})
